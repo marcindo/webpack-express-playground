1b99c18ada0d4c5cc83d38f17276896b381cfd36
marcindo <marcin.jakub.dolinski@gmail.com>
2018-01-22 13:05:17 +0100

    removing node modules

---
+++ src/components/root/root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Root() {
+  return (
+    <React.Fragment>
+      <h1>Hello, welcome to our test app</h1>
+      <a href="about">About  </a>
+      <a href="blog">Blog</a>
+    </React.Fragment>
+  );
+}
